bac6552298cd80ddbd758b8267b85da79271e09a
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-10-12 14:10:59 -0400

    solved inability to use backtic.

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -3,13 +3,30 @@ import "./HomeScreen.css";
 import Nav from "./Nav";
 
 function HomeScreen() {
-  return <div className='homeScreen'>
-    <Nav />
-    {/* Nav
+  return (
+    <div className="homeScreen">
+      <Nav />
+      {/* Nav
     Banner
     Row */}
-
-  </div>;
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+    </div>
+  );
 }
 
 export default HomeScreen;
