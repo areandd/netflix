7b7b8b104e0d1a7037f4f4877696f2fa6d6e82e8
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-10-12 12:25:33 -0400

    forming basic file struct

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img src="" alt="" />
+      <h1>nav</h1>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB6CuTDe02ssB9Jr7PpcY6SlXvhJXGPjUg",
+  authDomain: "netflix-e8f77.firebaseapp.com",
+  projectId: "netflix-e8f77",
+  storageBucket: "netflix-e8f77.appspot.com",
+  messagingSenderId: "258424529054",
+  appId: "1:258424529054:web:a862f4267c7509189b9b8f",
+  measurementId: "G-K928M57QL3",
+};
