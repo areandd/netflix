6a48edb4b05d64de043c22d2be00d2c434120cb0
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-10-12 14:53:07 -0400

    css for banner buttons complete.

---
+++ src/Banner.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeA8XKAAFZcBBuAAAAAElFTkSuQmCC")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <img src="" alt="" />
+      <div className="banner__contents">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">test description</h1>
+      </div>
+
+      <div className="banner--fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,30 +1,13 @@
 import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import Banner from "./Banner";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
-      {/* Nav
-    Banner
-    Row */}
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
+      <Banner />
     </div>
   );
 }
