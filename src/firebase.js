88f4873b66b1c9317df91451f798bfc02e7205fc
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-10-13 15:54:41 -0400

    firebase import corrected.

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
   apiKey: "AIzaSyB6CuTDe02ssB9Jr7PpcY6SlXvhJXGPjUg",
   authDomain: "netflix-e8f77.firebaseapp.com",
@@ -7,3 +11,10 @@ const firebaseConfig = {
   appId: "1:258424529054:web:a862f4267c7509189b9b8f",
   measurementId: "G-K928M57QL3",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {auth};
+export default db;
